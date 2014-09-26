864214ab5d52bac68e56e54eaa02ccfdb3ad5058
Tony McDaniel <tonymcdaniel@gmail.com>
2014-09-26 11:58:16 -0400

    added function to read wordlist

--- challenge.py
+++ challenge.py
@@ -15,8 +15,20 @@ Copyright (c) 2014 Tony R. McDaniel
 import numpy as np
 
 
+def read_wordlist(infile="randomlist.txt"):
+    """
+    Read data from `infile` and return a list
+    with each line as an element.
+    """
+    print("Loading wordlist from : "+infile)
+    with open(infile,'r') as f:
+        wordlist = [ w.strip() for w in f.readlines() ]
+    print("Loaded %d words."%len(wordlist))
+    return wordlist
+
+
 def main():
-    pass
+    wordlist = read_wordlist()
 
 
 if __name__ == '__main__':
