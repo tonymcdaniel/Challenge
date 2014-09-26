3e3bc7250b53f480a87e13a753f210212b024f77
Tony McDaniel <tonymcdaniel@gmail.com>
2014-09-26 11:52:25 -0400

    added python skeleton file

---
+++ challenge.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+File Name : challenge.py
+
+Author    : Tony McDaniel
+Contact   : tonymcdaniel@gmail.com
+
+Solution to challenge problem posted at
+https://github.com/waypaver/Challenge
+
+Copyright (c) 2014 Tony R. McDaniel
+
+"""
+import numpy as np
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
